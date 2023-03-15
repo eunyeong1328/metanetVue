1ec5bc723bf246c2a8024e08560082555fa1e54e
eunyoung1328 <eunyeong1328@daum.net>
2023-03-15 15:06:40 +0900

    use Function

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>{{name }}</div>
+  <div class = "name">
+    {{greeting()}}
+    </div>
 </template>
 
 <script>
 export default{
   setup(){
     const name = "Metanet!";
+    const greeting = () =>{
+      return "Hello MetaNet!";
+    }
 
     return{
       name,
+      greeting,
     }
   }
 }
 </script>
 
 <style>
-
+.name{
+  color: red;
+}
 </style>
