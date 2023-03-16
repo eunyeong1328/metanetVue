63a6b4719dc2b0a692b5378b4d7141e138f59db8
eunyoung1328 <eunyeong1328@daum.net>
2023-03-16 11:40:30 +0900

    :props 데이터 전달(부모->자식)

--- src/App.vue
+++ src/App.vue
@@ -6,37 +6,26 @@
   <div v-if = "!todos.length" style = "color: rebeccapurple">
     추가된 todo가 없습니다.
   </div>
-  
-  <!-- {{todos}} -->
-  <!-- v-for를 사용할 때 반드시 :key 사용 -->
-  <div v-for="(todo,index) in todos" :key="todo.id" class="card mt-2">
-    <div class = "card-body p-2 d-flex align-items-center">
-      <div class ="form-check flext-grow-1">
-        <input class = "form-check-input" type = "checkbox" v-model="todo.completed"> 
-        <label class = "form-check-label" :class = "{todo: todo.completed}"> {{ todo.subject }} </label>
-      </div>                        <!-- 스타일 바인딩 : 어떤 변수에 따라 스타일 변경 가능-->
-     <div class = "align-rignt">
-      <button class ="btn btn-danger btn-sm" @click = "deleteTodo(index)">
-        Delete
-      </button>
-     </div>
-    </div>
-  </div>
-</div> 
 
+  <TodoLis :todos="todos" />
+</div> 
 </template>
 
 <script>
 import { ref } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
-
+import TodoList from './components/TodoList.vue';
 export default{
-  components : {
-    TodoSimpleForm
+//부모 -> 자식
+  props : ['todos'],
+
+  components: {
+    TodoSimpleForm,
+    TodoList
   },
+
   setup(){
     // ref사용 시 .value값 선언 
-   
     const todos = ref([]);
 
     const deleteTodo = (index) => {
