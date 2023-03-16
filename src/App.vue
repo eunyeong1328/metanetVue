afb35d1b942a958eccef32995fa3144444935808
eunyoung1328 <eunyeong1328@daum.net>
2023-03-16 15:30:02 +0900

    서버에서 비동기 방식으로 처리

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@
   <h2>To-Do</h2><!--@add-todo이름 지정 후 "add-todo"로 호출될 함수-->
   <input class="form-control" type="text" v-model="searchText" placeholder="Search"/>
 <hr>
+<TodoSimpleForm @add-todo="addTodo"/>
+<div style="color: red">{{ error }}</div>
+
   <div v-if = "!todos.length" style = "color: rebeccapurple">
     추가된 todo가 없습니다.
   </div>
   <div v-if="!filterdTods.length">
     There is nothing to display
   </div>
-  <TodoSimpleForm @add-todo="addTodo"/>
   <!--   자식이 데이터 보내기         부모가 받을 데이터(데이터를 받을 함수)  -->
   <TodoList :todos="filterdTods" @toggel-todo = "toggleTodo" @toggel-del = "deleteTodo"/>
 </div> 
@@ -30,6 +32,7 @@ export default{
     // ref사용 시 .value값 선언 
     const todos = ref([]);
     const searchText = ref(''); //검색기능
+    const error = ref('');
     const filterdTods = computed(() => {
       if(searchText.value){
         return todos.value.filter(todo => {
@@ -40,13 +43,18 @@ export default{
       return todos.value;
     });
 
-    const addTodo = (todo) => {
-      //데이터를 객체 상태로 DB에 전달
-      axios.post('http://localhost:3000/todos', {
-        subject: todo.subject,
-        completed: todo.completed
-      })//<- 이 객체가 성공하면 
-      todos.value.push(todo);//<-여기에 데이터가 들어감
+    const addTodo = async (todo) => {
+      error.value = '';
+      try{ //await 이 작업을 먼저 실행
+        const res = await axios.post('http://localhost:3000/todos', {
+          subject: todo.subject,
+          completed: todo.completed
+        });
+        todos.value.push(res.data);
+      }catch(err){
+          console.log(err);
+          error.value = 'Something went wrong';
+      }
     }
 
     const toggleTodo = (index) => {
@@ -57,7 +65,6 @@ export default{
       todos.value.splice(index,1);
     }
 
-
 //함수를 사용하려면 반드시 return을 사용
     return{
       todos,
@@ -66,6 +73,7 @@ export default{
       toggleTodo,
       searchText,
       filterdTods,
+      error,
     }
   }
 }
