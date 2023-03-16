47c8f6e696dd069b39e91c561f5759b5b7f15dc9
eunyoung1328 <eunyeong1328@daum.net>
2023-03-16 09:43:15 +0900

    vue style binding 스타일 바인딩

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
   <form class="d-flex"  @submit.prevent="onSubmit">
     <div>
       <div class = "flex-grow-1 mr-2"> 
-        <input class = "form-control" type = "text" v-model="todo" placeholder="Type new to-do">
+        <input class = "form-control" type = "text" v-model="todo.completed" placeholder="Type new to-do">
+        
     </div>
     <div>
       <button class = "btn btn-primary" type="submit" >
@@ -24,8 +25,8 @@
   <div v-for="todo in todos" :key="todo.id" class="card mt-2">
     <div class = "card-body p-2">
       <div class ="form-check">
-        <input class = "form-check-input" type = "checkbox">
-        <label class = "form-check-label">{{ todo.subject }}</label>
+        <input class = "form-check-input" type = "checkbox" v-model="todo.completed"> <!-- 스타일 바인딩 : 어떤 변수에 따라 스타일 변경 가능-->
+        <label class = "form-check-label" :style="todoStyle ? todoStyle : {}"> {{ todo.subject }} </label>
       </div>
     </div>
   </div>
@@ -39,15 +40,15 @@ export default{
   setup(){
     // ref사용 시 .value값 선언 
     const todo = ref('');
-    const todos = ref([
-      {id:1, subject: 'vue study'},
-      {id:2, subject: 'vue work'}
-    ]);
-
+    const todos = ref([]);
     const hasError = ref(false);
+    const todoStyle = {
+     textdecoration: 'line-through',
+     color: 'gray' 
+    }
     const onSubmit = () => {
       //입력한 데이터 값
-        if(todo.value == ' '){
+        if(todo.value == ''){
           hasError.value = true;
         }else{
             todos.value.push({
@@ -56,6 +57,7 @@ export default{
             completed: false,
           }); //객체 형태로 생성
           hasError.value = false;
+          todo.value = '';
         }  
     }
 //함수를 사용하려면 반드시 return을 사용
@@ -63,6 +65,7 @@ export default{
       todo,
       onSubmit,
       todos,
+      todoStyle,
     }
   }
 }
