ed5c901f8d476952ec91a21e0a8d9b239fdbb2be
eunyoung1328 <eunyeong1328@daum.net>
2023-03-17 14:26:49 +0900

    feat: todo리스트이 상세 페이지 화면 구성과 데이터 전달

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -1,14 +1,44 @@
 <template>
-  <div>Todo pages</div>
+  <h1>Todo pages</h1>
+  <div v-if = "loading">
+    Loading...
+  </div>
+  <form v-else>
+    <div class = "form-group">
+      <label>Todo Subject</label> 
+      <!-- v-model todo를 양방향 바인딩 -->
+      <input v-model = "todo.subject" type = "text" class ="form-control">
+    </div>
+    <button class = "btn btn-primary">Save</button>
+  </form>
 </template>
 
 <script>
 import { useRoute } from 'vue-router';
+import axios from 'axios';
+import {ref} from '@vue/reactivity'
+
 export default {
     setup(){
         const route = useRoute();
-        console.log(route.params.id);
+        const todo = ref(null);
+        const loading = ref(true);
+        
+        //console.log(route.params.id);
+
+        const getTodo = async () =>{ 
+          const res = await axios.get('http://localhost:3000/todos/' + route.params.id);
+          //console.log(res);
+          todo.value = res.data;
+          loading.value = false; //db에서 데이터를 가져와야 만 
+        };
+        getTodo();
+        return{
+          todo,
+          loading,
+        };
     }
+    
 }
 </script>
 
