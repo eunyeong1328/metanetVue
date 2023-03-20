18822a17f42c79ff7abd9afd299e2d2d5bd71d2c
eunyoung1328 <eunyeong1328@daum.net>
2023-03-20 17:55:55 +0900

    fix: fix header button

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <router-link class="navbar-brand" :to = "{name:'Home'}">Home</router-link>
-  <ul class ="navbar-nav mr-auto">
+  <ul class ="navbar-nav mr-auto justify-content-between mb-3">
+    <li class = "nav-item active">
+      <router-link class="nav-link" :to = "{name:'Todos'}">Todos</router-link>
+    </li>
+
     <li class = "nav-item active">
       <router-link class="nav-link" :to = "{name:'Todos'}">Todos</router-link>
     </li>
   </ul>
 </nav> 
 <!-- 리로딩 안되게 하기 = 속도가 빨라짐 -->
+<div>
+  
+</div>
   <router-view/>
 </template>
 
 <script>
-export default {
+import HelloWorld from './components/HelloWorld.vue'
 
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
